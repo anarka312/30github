d520fab6ed3ac967106ebf0dcfbd337bb3ce9b1b
Anarka312 <natalie.talas@gmail.com>
2025-02-06 16:10:59 +0600

    add filters

---
+++ README 2.md
@@ -0,0 +1,2 @@
+# 30github
+next.js, tailwind
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
-import { Container, Title, SortPopup } from "@/components/shared";
-import { Categories } from "@/components/shared/categories";
+import { Container, Title } from "@/components/shared";
+import { TopBar } from "@/components/shared/top-bar";
+import { Filters } from "@/components/shared/filters";
 
 
 export default function Home() {
-  return <>
-  <Container className="mt-10">
-    <Title text="Все пиццы" size="lg" className="font-extrabold"/>
-    <Categories />
-    <SortPopup />
-  </Container>
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
 
- 
+      <TopBar />
+      <div style={{ height: "3000px" }} />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {/* <ProductGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+              <ProductGroupList title="Комбо" items={[1, 2, 3, 4, 5]} /> */}
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
-    
-  
+  );
 }
