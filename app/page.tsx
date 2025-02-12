214d8283b74f160fb84a7dbfe6feec8425fa976c
Anarka312 <natalie.talas@gmail.com>
2025-02-12 23:24:06 +0600

    use intersection observe

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { Container, Title } from "@/components/shared";
 import { TopBar } from "@/components/shared/top-bar";
 import { Filters } from "@/components/shared/filters";
-
+import Link from "next/link";
+import Image from "next/image";
 import { ProductsGroupList } from "@/components/shared/products-group-list";
 
 export default function Home() {
@@ -15,57 +16,121 @@ export default function Home() {
       {/* <div style={{ height: "3000px" }} /> */}
 
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           <div className="w-[250px]">
             <Filters />
           </div>
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList title="Пиццы" items={[
-                {
-                  id: 1,
-                  name: 'Чизбургер пицца',
-                  imageUrl: 'https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif',
-                  price: 550,
-                  items: [{price: 550}],
-                },
-                {
-                  id: 1,
-                  name: 'Чизбургер пицца',
-                  imageUrl: 'https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif',
-                  price: 550,
-                  items: [{price: 550}],
-                },
-                {
-                  id: 1,
-                  name: 'Чизбургер пицца',
-                  imageUrl: 'https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif',
-                  price: 550,
-                  items: [{price: 550}],
-                },
-                {
-                  id: 1,
-                  name: 'Чизбургер пицца',
-                  imageUrl: 'https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif',
-                  price: 550,
-                  items: [{price: 550}],
-                },
-                {
-                  id: 1,
-                  name: 'Чизбургер пицца',
-                  imageUrl: 'https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif',
-                  price: 550,
-                  items: [{price: 550}],
-                },
-                {
-                  id: 1,
-                  name: 'Чизбургер пицца',
-                  imageUrl: 'https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif',
-                  price: 550,
-                  items: [{price: 550}],
-                },
-              ]} categoryId={1} />
+              <ProductsGroupList
+                title="Пиццы"
+                items={[
+                  {
+                    id: 1,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1}
+              />
+              <ProductsGroupList
+                title="Завтраки"
+                items={[
+                  {
+                    id: 1,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1}
+              />
             </div>
           </div>
         </div>
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Title } from "./title";
 import { cn } from "@/lib/utils";
 import { ProductCard } from "./product-card";
+import { useIntersection } from "react-use";
 
 interface Props {
   title: string;
@@ -18,6 +19,15 @@ export const ProductsGroupList: React.FC<Props> = ({
   listClassName,
   categoryId,
 }) => {
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+  React.useEffect(() => {
+    if(intersection?.isIntersecting) {
+      console.log(title, categoryId)
+    }
+  }, [categoryId, intersection?.isIntersecting, title])
   return (
     <div className={className}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
