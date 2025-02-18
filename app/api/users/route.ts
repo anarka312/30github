e3289bb6e780050e722a5ec872b375282f75f965
Anarka312 <natalie.talas@gmail.com>
2025-02-18 16:30:46 +0600

    work with prisma seed

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
+
+  
   const users = await prisma.user.findMany();
   return NextResponse.json(users);
 }
